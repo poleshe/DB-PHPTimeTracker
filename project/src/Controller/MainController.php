8da2c654a02e1c83b77055ab41dca62e6f21c943
poleshe <polestecha14@gmail.com>
2020-08-02 16:54:47 +0200

    Finishing TimeTrack backend and ORM, commenting, adding the past task times viewer

--- project/src/Controller/MainController.php
+++ project/src/Controller/MainController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use DateTime;
 use App\Entity\Times;
 use App\Form\Times\FormTimes;
 
@@ -18,30 +19,61 @@ class MainController extends AbstractController
      */
     public function index(Request $request)
     {
-        $timetracker = new Times();
+        // Check if the request has been done (If the stop button has been pressed).
+        if ($request->request->get('name') && $request->request->get('time')) {
+            // Get the data.
+            $name = $request->request->get('name');
+            $time = $request->request->get('time');
+            // Start the doctrine manager.
+            $entityManager = $this->getDoctrine()->getManager();
+            // Query for one item with the same name, to check if it exists.
+            $timetracker = $entityManager->getRepository(Times::class)->findOneBy(array('name' => $name));
 
-        $form = $this->createForm(FormTimes::class, $timetracker);
+            // If it does not...
+            if (!$timetracker) {
+                // Initialize new Times ORM and set the name and datetime.
 
-        $timetracker = $form->getData();
+                $timetracker = new Times();
+                $timetracker->setName($name);
+                $timetracker->setTime(\DateTime::createFromFormat('h:i:s', $time));
 
-        var_dump($timetracker);
+                // tell Doctrine you want to (eventually) save the Product (no queries yet)
+                $entityManager->persist($timetracker);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $timetracker = $form->getData();;
-    
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
-    
-            return new Response('helysdasdlo', 200);
+                // Execute the query.
+                $entityManager->flush();
+            // If it DOES exist...
+            } else {
+                // Get the DB object time.
+                $timeA = $timetracker->getTime();
+                // Explode it to allow it's usage on DateInterval Object.
+                $explodedtime = array_map(function($num) {
+                    return (int) $num;
+                }, explode(':', $time));
+                // Create a new DateInterval. This object contains the elapsed time of the time tracker. We create this object so we can use the add method.
+                $timeB = new \DateInterval('PT'.$explodedtime[0].'H'.$explodedtime[1].'M'.$explodedtime[2].'S');
+                $timeA->add($timeB);
+                // Set the name and new date time.
+                $timetracker->setName($name);
+                $timetracker->setTime(\DateTime::createFromFormat('H:i:s', $timeA->format('H:i:s')));
+
+                // Execute the query.
+                $entityManager->flush();
+                
+            }
+            // We are done. Return to the time tracker.
+            $alltimes = $this->getDoctrine()->getRepository(Times::class);
+            $alltimes = $alltimes->findAll();
+            return $this->render('base.html.twig', array(
+                'times' => $alltimes,
+            ));
         } else {
-            return $this->render('base.html.twig');
-        }
-
-        
+            // If no petition has been made we just go to the main page.
+            $alltimes = $this->getDoctrine()->getRepository(Times::class);
+            $alltimes = $alltimes->findAll();
+            return $this->render('base.html.twig', array(
+                'times' => $alltimes,
+            ));
+        }        
     }
 }
